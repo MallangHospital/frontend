ce98cf9a75faed4969c364819852828f07ab41b3
do2y <essen030216@naver.com>
2024-11-14 20:55:20 +0900

    수정중

--- script.js
+++ script.js
@@ -6,11 +6,3 @@ $(function(){
     $("#footer").load("footer.html");
 })
 
-// 페이지별로 필요한 스크립트는 이런 식으로 조건문 만들어서 코드 작성 - contains('클래스이름')
-if (document.body.classList.contains('login-page')) {
-    console.log("This is the login page.");
-}
-
-if (document.body.classList.contains('main-page')) {
-    console.log("This is the main page.");
-}
